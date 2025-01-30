00c8ff6a774a4c84ec9032654c5940850ad4dd04
Hrishikesh778 <hrishi778@gmail.com>
2025-01-30 15:48:50 +0530

    log reg fix

--- vite-loginreg/src/Hooks/Registration.jsx
+++ vite-loginreg/src/Hooks/Registration.jsx
@@ -2,8 +2,8 @@ import { useState } from "react"
 import "./Registration.css"
 function Registration() {
 
-    let [fname, setfname] = useState();
-    let [lname,setlname]=useState("")
+    let [fname, setfname] = useState("");
+    let [lname, setlname] = useState("")
     let [email, setEmail] = useState("");
 
     let [number, setNumber] = useState("");
@@ -12,32 +12,32 @@ function Registration() {
     let [cpwd, setcPwd] = useState("");
 
     let regex =
-                /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.#$!%*?&])[A-Za-z\d@.#$!%*?&]{8,15}$/;
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.#$!%*?&])[A-Za-z\d@.#$!%*?&]{8,15}$/;
 
     function submitData(e) {
         e.preventDefault()
         console.log('first')
         // console.log(fname,lname, number, email, pwd ,cpwd)
 
-        if (fname == "" || lname == "" || email == "" || number == "" || pwd == "" || cpwd == "") {
-            alert("please fill all fields")
-        }
-        else if(email.includes('@')==false){
-            alert("please enter proper email")
-        }
-        else if(number.length !== 10){
-            alert("enter proper number")
-        }
-        else if(regex.test(pwd) != true){
-            alert("enter proper password")
-        }
-        else if(regex.test(pwd) != true){
-            alert("enter proper password")
-        }
-        else if (cpwd != pwd) {
-            alert("your password do not match")
-        
-        }
+        // if (fname == "" || lname == "" || email == "" || number == "" || pwd == "" || cpwd == "") {
+        //     alert("please fill all fields")
+        // }
+        // else if(email.includes('@')==false){
+        //     alert("please enter proper email")
+        // }
+        // else if(number.length !== 10){
+        //     alert("enter proper number")
+        // }
+        // else if(regex.test(pwd) != true){
+        //     alert("enter proper password")
+        // }
+        // else if(regex.test(pwd) != true){
+        //     alert("enter proper password")
+        // }
+        // else if (cpwd != pwd) {
+        //     alert("your password do not match")
+
+        // }
     }
 
     return (
@@ -54,62 +54,78 @@ function Registration() {
                 <div id="logbox">
 
                     <form>
-                        <br/>
+                        <br />
                         <h3>Registration</h3>
-                        
+
                         <div className="inmain">
                             <div className="div">
                                 <b><label >First Name</label></b><br />
-                                <input type="text" placeholder="Enter your name" id="fname" onChange={(e)=>{setfname(e.target.value)}} />
+                                <input type="text" placeholder="Enter your name" id="fname" onChange={(e) => { setfname(e.target.value) }} />
                                 {/* {fname==""?<b>Fill the details</b>:""} */}
-                                <span className="lfname" style={{display:"none",color:"red"}}>fill the name</span>
+
                             </div>
                             <div className="sep">
 
                             </div>
-                            
+
                             <div className="div">
                                 <b><label >Last Name</label></b><br />
-                                <input type="text" placeholder="Enter your username" id="lname" onChange={(e)=>{setlname(e.target.value)}} />
+                                <input type="text" placeholder="Enter your username" id="lname" onChange={(e) => { setlname(e.target.value) }} />
                             </div>
                         </div>
                         <div className="inmain">
                             <div className="div">
                                 <b><label >Email</label></b><br />
-                                <input type="email" placeholder="Enter your email" id="email" onChange={(e)=>{setEmail(e.target.value)}} />
+                                <input type="email" placeholder="Enter your email" id="email" onChange={(e) => { setEmail(e.target.value) }} />
+                                {
+                                    (email.includes('@') == false) && (email != "") ? <p className='miniText'>Plesae include @ in your email</p> : ""
+                                }
                             </div>
                             <div className="sep">
 
                             </div>
                             <div className="div">
                                 <b><label >Phone Number</label></b><br />
-                                <input type="number" placeholder="Enter your number" id="number" onChange={(e)=>{setNumber(e.target.value)}} />
+                                <input type="number" placeholder="Enter your number" id="number" onChange={(e) => { setNumber(e.target.value) }} />
+                                {
+                                    (number.length != 10) && (number != "") ? <p className='miniText'>Valid Mobile number consist 10 digit</p> : ""
+                                }
                             </div>
                         </div>
 
                         <div className="inmain">
                             <div className="div">
                                 <b><label >Password</label></b><br />
-                                <input type="password" placeholder="Enter your password" id="pass" onChange={(e)=>{setPwd(e.target.value)}} />
+                                <input type="password" placeholder="Enter your password" id="pass" onChange={(e) => { setPwd(e.target.value) }} />
+                                {
+                                    (regex.test(pwd) != true) && (pwd != "") ? <p className='miniText'>Password must contain one special char and uppercase letter</p> : ""
+                                }
                             </div>
                             <div className="sep">
 
                             </div>
                             <div className="div">
                                 <b><label >Confirm Password</label></b><br />
-                                <input type="password" placeholder="Confirm your password" id="cpass" onChange={(e)=>{setcPwd(e.target.value)}}  />
+                                <input type="password" placeholder="Confirm your password" id="cpass" onChange={(e) => { setcPwd(e.target.value) }} />
+                                {
+
+                                    (pwd != cpwd) && (cpwd != "") ? <p className='miniText'>Password and Confirm Password are not same+</p> : ""
+
+                                }
                             </div>
                         </div>
-                 
+
 
                         <div className="inmain">
-                            <input type="submit" id="submit" onClick={(e)=>{submitData(e)}} />
-
+                            
+                            {
+                                (fname == "") || (lname == "") || (email == "") || (number == "") ||  (pwd == "") || (cpwd == "") ? <input type="submit" id="submit2" disabled={true}/> : <input type="submit" id="submit" onClick={(e) => { submitData(e) }} />
+                            }
 
                         </div>
 
                         <br />
-                        
+
 
                     </form>
 
